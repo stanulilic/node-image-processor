7f80447921e29e32f2e6280a11dd785eba916128
Stanley Ulili <stanleyulili@gmail.com>
2021-06-26 20:54:57 +0200

    (feat): force user to enter a valid directory path

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
+const fs = require("fs");
 const readlineSync = require("readline-sync");
 
-var directoryPath = readlineSync.question(
-  `========================================================
-   Enter the full path of the directory containing images.\n
-   example: /home/username/pics
+const getDirectoryPath = () => {
+  let directoryPath = null;
+
+  directoryPath = readlineSync.question(
+    `========================================================
+    Enter the full path of the directory containing images.\n
+    example: /home/username/pics
+    ...`
+  );
+
+  while (!fs.existsSync(directoryPath)) {
+    directoryPath = readlineSync.question(
+      `The directory path is invalid. please enter a valid directory path.\n
    ...`
-);
+    );
+  }
+
+  return directoryPath;
+};
+
+const dirPath = getDirectoryPath();
+console.log(dirPath);
